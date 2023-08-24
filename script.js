92c5d86110e4f62f035fb7b72222f64d7465544a
mihgon <mih_gon@hotmail.com>
2023-08-24 10:09:50 -0300

    Inclusão de textos alternativos

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
+const titulo = document.querySelector('.app__title')
 
 focoBt.addEventListener('click', () => {
     alterarContexto('foco')
@@ -19,4 +20,23 @@ longoBt.addEventListener('click', () => {
 function alterarContexto (contexto) {
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src', `/imagens/${contexto}.png`)
+    switch (contexto) {
+        case "foco":
+            titulo.innerHTML = `
+            Otimize sua produtividade,<br><strong class="app__title-strong">mergulhe no que importa.</strong>`
+            break;
+        case "descanso-curto":
+            titulo.innerHTML = `
+            Que tal dar uma respirada? <br><strong class="app__title-strong">Faça uma pausa curta!</strong>`
+            break;
+        case "descanso-longo":
+            titulo.innerHTML = `
+            Hora de voltar à superfície. <br><strong class="app__title-strong">Faça uma pausa longa.</strong>`
+            break;
+        default:
+            break;
+
+            
+
+    }
 }
